679b15472bb788b8e1bd067bf55926746d1656a6
John <john.daglas.dev@gmail.com>
2026-05-19 12:31:47 -0700

    feat: add analytics

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import css from './App.module.scss';
 import { CheckoutLab } from '../CheckoutLab/CheckoutLab';
+import { Analytics } from '@vercel/analytics/react';
 
 export const App = () => {
   return (
     <div className={css.wrapper}>
       <CheckoutLab />
+      <Analytics />
     </div>
   );
 };
